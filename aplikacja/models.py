1db9a3c18e0913df0fadcaa7d7c8f86de8de931e
Kinga Smolarek <100038018+Radio2840@users.noreply.github.com>
2024-12-16 10:26:49 +0100

    Modele zmiany itd

--- aplikacja/models.py
+++ aplikacja/models.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 class User(Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, default=0)
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -16,11 +17,18 @@ class User(Model):
 
 
 class Animals(Model):
+    STATUS_CHOICES = (
+        ('adoptowany', 'Adoptowany'),
+        ('nieAdoptowany', 'NieAdoptowany')
+    )
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=100)
     animal_race = models.CharField(max_length=100)
     age = models.CharField(max_length=200)
     description = models.CharField(max_length=400)
     dodano = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=20,
+                             choices=STATUS_CHOICES,
+                             default='nieAdoptowany')
 
 # Create your models here.
